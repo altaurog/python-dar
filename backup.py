1636fe8332c0a172ff30da655eceebafdc32a112
Aryeh Leib Taurog <git@aryehleib.com>
2011-10-26 12:16:03 +0200

    remove iterpipes dependency, refactor with oo

--- backup.py
+++ backup.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
+# requires python 2.6 - 3.x
+from __future__ import print_function
+
 import os
+import subprocess
 import socket
 
 from datetime import datetime
 
-from iterpipes import call, cmd
-
+try:
+    from shlex import quote
+except ImportError:
+    from pipes import quote
 
 """
 TODO:
@@ -17,7 +23,6 @@ TODO:
     upload selected archives (home dirs) to amazon s3
     backup remote systems, both linux and windows
     build dar 2.4.2 and use that
-    don't use iterpipes
 
     monthly/weekly TOH, start on first Sat of month:
 
@@ -39,13 +44,6 @@ def get_timestamp():
     fmt = '%Y%m%d_%H%M'
     return now.strftime(fmt)
 
-def make_command(command, args=[], logfile=None):
-    args = [command] + args
-    template = ' '.join(['{}'] * len(args))
-    if logfile:
-        template += ' > ' + logfile
-    return cmd(template, *args)
-
 def get_dcf():
     confdir = os.path.join(os.path.dirname(__file__), 'config')
     dcf_dict = {}
@@ -57,37 +55,66 @@ def get_dcf():
 
     return dcf_dict
 
-def backup(batch, reference=None):
-    timestamp_str = get_timestamp()
-    kwargs = dict(ts=timestamp_str, b=batch, h=socket.gethostname())
-    path = '/backup/dar/{ts}/{h}-{b}/'.format(**kwargs)
+def checkpath(*args):
+    path = os.path.join(*args)
     if not os.path.exists(path):
         os.makedirs(path)
+    return path
 
-    basename = '{h}-{b}-{ts}'.format(**kwargs)
-    catalog = '{0}-catalog'.format(basename)
-    logfile = '{0}-log'.format(basename)
+def quote_command(cmd, *args):
+    return cmd + ' '.join(map(quote, args))
 
-    args = [
-        # create an archive
-        '--create', os.path.join(path, basename),
+class BackupSession(object):
+    def __init__(self, path):
+        self.sid = get_timestamp()
+        self.path = checkpath(path, self.sid)
+        logfile = os.path.join(self.path, 'log')
+        self.log = open(logfile, 'w')
 
-        # create a isolated catalog on the fly
-        '--on-fly-isolate', os.path.join(path, catalog),
-    ]
+    def flushlog(self):
+        self.log.flush()
+        os.fsync(self.log.fileno())
 
-    dcf = get_dcf()
-    for b in ('c-base', 'encryptionkey', 'compression', batch):
-        args.extend(['--batch', dcf[b],])
+    def print_f(self, template, *args, **kwargs):
+        print(template.format(*args, **kwargs), file=self.log)
+        self.flushlog()
 
-    if reference:
-        # Reference previous backup archive/catalog
-        args.extend(['--ref', reference])
+    def backup(self, name, reference=None):
+        host = socket.gethostname()
+        path = checkpath(self.path, host, name)
 
-    call(make_command('dar', args, logfile=os.path.join(path, logfile)))
+        basename = '-'.join((host, name, self.sid))
+        catalog = 'catalog-' + basename
 
+        args = [
+            'dar',
+
+            # create an archive
+            '--create', os.path.join(path, basename),
+
+            # create a isolated catalog on the fly
+            '--on-fly-isolate', os.path.join(path, catalog),
+        ]
+
+        dcf = get_dcf()
+        for b in ('c-base', 'encryptionkey', 'compression', name):
+            args.extend(['--batch', dcf[b],])
+
+        if reference:
+            # Reference previous backup archive/catalog
+            args.extend(['--ref', reference])
+
+        self.log_execution(args)
+
+    def log_execution(self, args):
+            self.print_f('executing command:\n' + quote_command(*args))
+            return_code = subprocess.call(args,
+                                        stdout = self.log,
+                                        stderr = subprocess.STDOUT)
+            self.print_f("{0} returned: {1:d}\n", args[0], return_code)
 
 if __name__ == '__main__':
-    backup('home')
-    backup('system')
+    session = BackupSession('/backup/dar')
+    session.backup('home')
+    session.backup('system')
 
